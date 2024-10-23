995dd0c686eca13391f6b922bb6c3d59cecbc41c
Alemoretti <alemoretti10@gmail.com>
2024-10-23 05:34:16 -0300

    Add flash messages

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout title="Series">
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Create</a>
+    @isset( $successMessage )
+        <div class="alert alert-success">
+            {{ $successMessage }}
+        </div>
+    @endisset
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">{{ $serie->name }}
